6b704d99ab58b69c177ae6bf84f5713ecd26cf1e
Xie <xiey0@icloud.com>
2024-05-04 13:31:59 +0800

    Format

--- Chat/Server.cs
+++ Chat/Server.cs
@@ -7,21 +7,26 @@ namespace Chat;
 
 class Program
 {
+    static async void Accept(){
+        
+    }
     static void Main(string[] args)
     {
         Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         IPAddress ip = IPAddress.Parse("127.0.0.1");
         s.Bind(new IPEndPoint(ip, 2300));
-        s.Listen(20);
+        s.Listen(100);
         while (true)
         {
+
             Socket temp = s.Accept();
-            string recvStr = "";
-            byte[] recvBytes = new byte[1024];
+            string receiveStr = "";
+            byte[] receiveBytes = new byte[1024];
             int bytes;
-            bytes = temp.Receive(recvBytes, recvBytes.Length, 0);
-            recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
-            Console.WriteLine(recvStr);
+            bytes = temp.Receive(receiveBytes, receiveBytes.Length, 0);
+            receiveStr += Encoding.ASCII.GetString(receiveBytes, 0, bytes);
+            Console.WriteLine(receiveStr);
+            temp.Close();
         }
 
 
--- Client/Client.cs
+++ Client/Client.cs
@@ -12,37 +12,42 @@ class Program
 
     static void Main(string[] args)
     {
-        
+
         Console.WriteLine("Please write down your name.\n请写下你的名字。");
-        start:
+    start:
         string name = Console.ReadLine();
-        if(name!=""){
+        if (name != "")
+        {
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2300);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2300);
 
-                s.Connect(iPEndPoint);
+            s.Connect(iPEndPoint);
 
-                string str = $"====== Welcome {name}! ======";
-                byte[] a = Encoding.UTF8.GetBytes(str);
-                s.Send(a, 0);
-                s.Close();
+            string str = $"====== Welcome {name}! ======";
+            byte[] a = Encoding.UTF8.GetBytes(str);
+            s.Send(a, 0);
+            s.Close();
             while (true)
             {
+                string str2 = name + ": " + Console.ReadLine()!;
+
                 Socket s2 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 IPEndPoint iPEndPoint2 = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2300);
 
                 s2.Connect(iPEndPoint);
 
-                string str2 = name + ": " + Console.ReadLine()!;
                 byte[] a2 = Encoding.UTF8.GetBytes(str2);
                 s2.Send(a2, 0);
                 s2.Close();
+            
             }
-        }else{
+        }
+        else
+        {
             Console.WriteLine("Don't leave it empty. ");
             goto start;
         }
-        
+
 
 
 
